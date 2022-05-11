8588174f0702572f9e91bd9229eb4b2aace2ab7a
tffaruk <faruk.themefisher@gmail.com.com>
2022-05-11 12:25:09 +0600

    not found

--- pages/500.js
+++ pages/500.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const Error = () => {
+  const router = useRouter();
   return <div>Error</div>;
 };
 
 export default Error;
+export const getStaticProps = async () => {
+  const notFoundData = getSingleFile("content/404.md");
+
+  return {
+    props: {
+      notFoundData: notFoundData,
+    },
+    notFound: true,
+    redirect: {
+      destination: "/404",
+      permanent: false,
+    },
+  };
+};
